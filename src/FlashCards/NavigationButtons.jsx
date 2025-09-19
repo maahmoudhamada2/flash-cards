a9ed430e0816620055302b8f543701187763a03a
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-09-19 16:29:56 +0300

    feat: add NavigationButtons component

---
+++ src/FlashCards/NavigationButtons.jsx
@@ -0,0 +1,21 @@
+export default function NavigationButtons({
+  setQuestionIdx,
+  showAnswer,
+  setShowAnswer,
+}) {
+  const handleNextClick = () => {
+    setQuestionIdx((prev) => (prev === 19 ? 19 : prev + 1));
+  };
+  const handlePreviousClick = () => {
+    setQuestionIdx((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+  return (
+    <>
+      <button onClick={handlePreviousClick}>&lt; Previous</button>
+      <button onClick={() => setShowAnswer((prev) => !prev)}>
+        {showAnswer ? "Hide Answer" : "Show Answer"}
+      </button>
+      <button onClick={handleNextClick}>Next &gt;</button>
+    </>
+  );
+}
